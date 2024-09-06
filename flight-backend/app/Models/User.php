e69d9284f862b7af46b48087472d0f7d066166dc
Pavle Dimitrijevic <dimitrijevicc.p@gmail.com>
2024-09-06 18:29:01 +0200

    Update za modele

--- flight-backend/app/Models/User.php
+++ flight-backend/app/Models/User.php
@@ -11,32 +11,16 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    protected $primaryKey = 'user_id';
+
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'name', 'email', 'password', 'role',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
-        'password',
-        'remember_token',
+        'password', 'remember_token',
     ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
+   
     protected function casts(): array
     {
         return [
