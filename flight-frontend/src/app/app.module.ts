b84fa14f932c85414ebef35cbd7fbb37401aa337
Pavle Dimitrijevic <dimitrijevicc.p@gmail.com>
2024-09-11 15:00:31 +0200

    Dodate pages login, register, book-flight

---
+++ flight-frontend/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { BookFlightComponent } from './pages/book-flight/book-flight.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BookFlightComponent,
+    LoginComponent,
+    RegisterComponent,
+    // Dodaj sve komponente ovde
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    // Dodaj sve druge module ovde
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
